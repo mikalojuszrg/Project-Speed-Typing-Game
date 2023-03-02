c21a82ebba07f459c7bb15dbb838a73667d2e261
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-02 17:30:15 +0200

    added time tracking

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,21 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [trackText, setTrackText] = useState("");
   const [countdown, setCountdown] = useState(5);
+  const [startTimer, setStartTimer] = useState(false);
 
   const handleTextTracking = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTrackText(e.target.value);
   };
 
   useEffect(() => {
-    if (countdown > 0) {
-      const timeout = setTimeout(() => {
+    if (countdown > 0 && startTimer) {
+      setTimeout(() => {
         setCountdown((prevTime) => prevTime - 1);
       }, 1000);
-
-      return () => clearTimeout(timeout);
+    } else if (countdown === 0) {
+      setStartTimer(false);
     }
-  }, [countdown]);
+  }, [countdown, startTimer]);
 
   const countWord = (text: string): number => {
     return text
@@ -30,8 +31,8 @@ const App = () => {
       <h1>How fast do you type?</h1>
       <textarea onChange={handleTextTracking} />
       <h4>Time reminaing: {countdown}</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
+      <button onClick={() => setStartTimer(true)}>Start</button>
+      <h1>Word count: {countdown === 0 && countWord(trackText)}</h1>
     </div>
   );
 };
